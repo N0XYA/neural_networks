8f0288cfc988fedd1c5747bd0535047e86438f48
Kirill <kpicmo@mail.ru>
2023-09-05 11:37:32 +0300

    added xlsx

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print('hello_git')
+import pandas as pd
+
+k = 1 #iterations
+
+df = pd.read_excel('training_data.xlsx')
+
+
+w1 = 0
+w2 = 0
+def training(df):
+    x1 = [value[0] for value in df.values]
+    x2 = [value[1] for value in df.values]
+    y = [value[2] for value in df.values]
+    return
+
+
+def main():
+    training(df)
+    return 0
+
+
+if __name__ == "__main__":
+    main()
