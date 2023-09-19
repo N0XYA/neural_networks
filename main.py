e1da24cbe03e58bffadc15907e882d608c83805a
Kirill <kpicmo@mail.ru>
2023-09-19 11:26:28 +0300

    1 пр готова

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import pandas as pd
 
-k = 1 #iterations
-
 df = pd.read_excel('training_data.xlsx')
+df = df.values.tolist()
 
-
-def OUT(NET):
+def OUT(x1, x2, w1, w2, t):
+    NET = w1*x1 + w2*x2 - t
     if NET > 0:
         return 1
     else:
@@ -13,31 +12,33 @@ def OUT(NET):
 
 
 def training(df):
-    T = 0
-    k = 4
-    w1 = 0
-    w2 = 0
-    x1_values = [value[0] for value in df.values]
-    x2_values = [value[1] for value in df.values]
-    # y_values = [value[2] for value in df.values]
-    for i in range(k):
-        y = OUT(x)
-        x1 = x1_values[i]
-        x2 = x2_values[i]
-        w1 = w1 + x1 * y
-        w2 = w2 + x2 * y
-        T = T - y
-        NET = 1 if x1 == x2 else 0
-        # NET = OUT(NET)
-        print("i=", i, "x1=", x1, "x2=", x2, "y=", y)
-        print("w1 =", w1, "w2=", w2, "T=", T, "NET=", NET)
-        print('\n')
-    return w1, w2
+    x1 = [row[0] for row in df]
+    x2 = [row[1] for row in df]
+    y = [row[2] for row in df]
+    w1, w2 = 0, 0
+    t = 0
+    k = 0
+    for k in range(4):
+        w1 = w1 + x1[k] * y[k]
+        w2 = w2 + x2[k] * y[k]
+        t = t - y[k]
+        print("w1:", w1)
+        print("w2:", w2)
+        print("t:", t)
+        print("NET:", OUT(x1[k], x2[k], w1, w2, t))
+        print("=============")
 
+    print("Введите данные для проверки:")
+    print("x1:")
+    test_x1 = input()
+    print("x2:")
+    test_x2 = input()
+    print("y:", OUT(int(test_x1), int(test_x2), w1, w2, t))
+    return
 
 def main():
-    w1, w2 =training(df)
-    print(w1, w2)
+    training(df)
+
     return 0
 
 
