d7ac4ae22906c740669c8a1f094dec7470ef7cb2
Kirill <kpicmo@mail.ru>
2023-09-30 00:18:31 +0300

    deltarule done

--- DeltaRule.py
+++ DeltaRule.py
@@ -1,42 +1,41 @@
 import pandas as pd
 
-df = pd.read_excel('training_data.xlsx')
+df = pd.read_excel('delta_rule.xlsx')
 df = df.values.tolist()
 
+def out(net):
+    return 1 if net >= 2 else -1
 
-def OUT(x1, x2, w1, w2, t):
-    NET = w1*x1 + w2*x2 - t
-    if NET > 0:
-        return 1
-    else:
-        return -1
+
+def net(x1, x2, w1, w2, t, y):
+    return x1 * w1 + x2 * w2 - y * t
+
 
 def training():
     x1 = [row[0] for row in df]
     x2 = [row[1] for row in df]
-    x3 = -1
     y = [row[2] for row in df]
-    w1, w2 = 0, 0
-    t = 0
     n = 1
-    e = []
+    e = 0
+    w1 = 0
+    w2 = 0
+    t = 0
     for i in range(len(x1)):
-        print(i)
-        e.append(y[i] - OUT(x1[i], x2[i], w1, w2, t))
-        w1 = w1 + n * (y[i] - OUT(x1[i], x2[i], w1, w2, t)) * x1[i]
-        w2 = w2 + n * (y[i] - OUT(x1[i], x2[i], w1, w2, t)) * x2[i]
-        t = t + n * (y[i] - OUT(x1[i], x2[i], w1, w2, t)) * x3
-        print("w1", w1)
-        print("w2", w2)
-        print("out", OUT(x1[i], x2[i], w1, w2, t))
-        print("===============")
-    print(w1, w2)
-    print(e)
-    print("Test x1")
-    testx1 = input()
-    print("test x2")
-    testx2 = input()
-    print("y:", OUT(int(testx1), int(testx2), w1, w2, t))
+        print("x1=", x1[i], "x2=", x2[i], "y=", y[i])
+        s = net(x1[i], x2[i], w1, w2, t, y[i])
+        pred = out(s)
+        print("net=", s, "y pred=", pred)
+        w1 = w1 + n * (y[i] - pred) * x1[i]
+        w2 = w2 + n * (y[i] - pred) * x2[i]
+        t = t + n * (y[i] - pred) * t
+        print("w1=", w1, "w2=", w2)
+        print("t=", t)
+        print("=========")
+
+    print("TEST")
+    test1 = int(input("x1:"))
+    test2 = int(input("x2:"))
+    print(out(test1*w1 + test2*w2))
 def main():
     training()
 
