4bf9a96ecfab9183a7b2e01cd719556d4df7e4a4
Kirill <kpicmo@mail.ru>
2023-10-28 02:32:36 +0300

    working on bp nn

--- back_prop/bp_V2/test.py
+++ back_prop/bp_V2/test.py
@@ -37,30 +37,66 @@ class nn:
         return e_x / e_x.sum(axis=0)  # only difference
 
     def train(self):
-        num_of_epoches = 1
+        num_of_epoches = 10
         step = 0.001
         input_weights = np.load("inp_w.npy")
         hidden_weights = np.load("hid_w.npy")
         bias = np.load("bias.npy")
-        print("weights loaded!")
         # self.generate_start_weights()
+        print("weights loaded!")
+
 
         for epoch in range(num_of_epoches):
-            if epoch % 10 == 0:
-                print("epoch:", epoch)
+            print("epoch:", epoch)
+            # if epoch % 5 == 0:
+            #     print("epoch:", epoch)
 
             for image_num in range(len(self.inputs)):
-                list_for_softmax = []
                 inp_layer = self.inputs[image_num]
                 label = self.labels[image_num]
+                expected_out = [1 if label == _ else 0 for _ in range(10)]
+
                 hidden_layer = []
+                # inputs to hidden layer
                 for neuron_num in range(self.hidden_layer_size):
                     hidden_out = []
                     for input_num in range(len(inp_layer)):
                         hidden_out.append((inp_layer[input_num] * input_weights[neuron_num][input_num]) + 1)
                     hidden_out = self.softmax(hidden_out)
                     hidden_layer.append(max(hidden_out))
-                print(len(hidden_layer))
+
+                # hidden layer to outputs
+                output = []
+                for neuron_num in range(10):
+                    out = []
+                    for input_num in range(self.hidden_layer_size):
+                        out.append((hidden_layer[input_num]*hidden_weights[neuron_num][input_num]) + 1)
+                    out = self.softmax(out)
+                    output.append(max(out))
+
+
+                # error
+                # sigmas
+                errors = []
+                out_sigmas = []
+                omega = [0 for _ in range(self.hidden_layer_size)]
+                for i in range(10):
+                    error = (expected_out[i] - output[i])**2 / 2
+                    sigma = error * output[i] * (1 - output[i])
+                    out_sigmas.append(sigma)
+                    for weight_num in range(len(hidden_weights[i])):
+                        # updating hidden layer weights
+                        #MAYBE change raws in place
+                        hidden_weights[i][weight_num] -= step * sigma * hidden_layer[weight_num]
+                        omega[weight_num] += sigma * hidden_weights[i][weight_num]
+
+                for i in range(self.hidden_layer_size):
+                    for weight_num in range(len(input_weights[i])):
+                        input_weights[i][weight_num] -= step * omega[i] * inp_layer[weight_num]
+
+            np.save("inp_w", input_weights)
+            np.save("hid_w", hidden_weights)
+            print("weights saved!")
 
 
 def main():
