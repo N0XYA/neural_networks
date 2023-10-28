dc7b99fb4232d188c5e5c4698b95e2fa3ab3961b
Kirill <kpicmo@mail.ru>
2023-10-28 18:27:41 +0300

    MNIST CLASSIFICATION BACK PROPAGATION DONE!!!!!!

---
+++ back_prop/bp_V2/v3.py
@@ -0,0 +1,62 @@
+import numpy as np
+import random
+import pandas as pd
+
+
+def f(x):
+    return 2 / (1 + np.exp(-x)) -1
+
+
+def df(x):
+    return 0.5 * (1 + x) * (1 - x)
+
+
+hl_size = 150
+w1 = np.array([[random.uniform(-0.5, 0.5) for _ in range(784)] for _ in range(hl_size)])
+w2 = np.array([[random.uniform(-0.5, 0.5) for _ in range(hl_size)] for _ in range(10)])
+
+
+def go_forward(inp):
+    summ = np.dot(w1, inp)
+    out = np.array([f(x) for x in summ])
+
+    summ = np.dot(w2, out)
+    y = np.array([f(x) for x in summ])
+    # y = f(summ)
+    return (y, out)
+
+
+def train(epoch):
+    global w2, w1, hl_size
+    step = 0.001
+    for i in range(len(epoch)):
+        # print(i)
+        expected = [1 if epoch[i][0] == _ else 0 for _ in range(10)]
+        x = epoch[i]
+        y, out = go_forward(x[1:])
+        e = y - expected
+        delta = np.array([e[i]*df(y[i]) for i in range(10)])
+        sigma = [0 for _ in range(hl_size)]
+        for n in range(10):
+            w2[n] = w2[n] - step * delta[n] * y[n]
+            for k in range(hl_size):
+                sigma[k] += w2[n][k] * delta[n]
+
+        for n in range(hl_size):
+            w1[n] = w1[n] - np.array(x[1:]) * sigma[n] * step
+
+        if i % 1000 == 0:
+            y = list(y)
+            max_v = max(y)
+            print("epoch ", i)
+            # print(f"NUMBER {epoch[i][:1]}")
+            print("Expected ", epoch[i][:1])
+            print("got", y.index(max_v))
+            print("======")
+
+
+    return
+
+
+set_of_numbers = pd.read_csv("../mnist_train.csv").values.tolist()
+train(set_of_numbers)
