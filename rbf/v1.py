16e8543ba1dfaa92bb5f8b41bc62c481678c5d24
Kirill <kpicmo@mail.ru>
2023-11-01 23:44:35 +0300

    made nn for rtl :)

--- rbf/v1.py
+++ rbf/v1.py
@@ -2,5 +2,12 @@ import tensorflow_datasets as tfds
 
 
 ds = tfds.load("cats_vs_dogs", split="train")
-for ex in ds.take(4):
-    print(ex)
+ds = ds.take(1)
+for example in ds:
+    print(list(example.keys()))
+    print(type(example))
+    image = example["image"]
+    label = example["label"]
+    print(image.shape, label)
+    print(type(image))
+    
